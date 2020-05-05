75fc1377eafa766c52798d36c6ef5b0577b568a8
Fortygin <slavafortygin785@gmail.com>
2020-05-05 15:26:53 +0500

    added if else

--- LearnCsharp/LearnCsharp/Program.cs
+++ LearnCsharp/LearnCsharp/Program.cs
@@ -8,9 +8,10 @@ namespace LearnCsharp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            while(true)
+            while (true)
             {
                 Console.Clear();
                 Console.WriteLine("What is your name?");
@@ -31,9 +32,9 @@ namespace LearnCsharp
                 Console.WriteLine(sum + " - sum of three numbers");
                 Console.WriteLine(product + " - product of three numbers");
 
-                
 
-                int userValue1; 
+
+                int userValue1;
                 int userValue2;
                 int userValue3;
 
@@ -43,10 +44,50 @@ namespace LearnCsharp
 
                 string Value1 = Console.ReadLine();
                 int.TryParse(Value1, out userValue1);
+
+                bool resultValue1 = int.TryParse(Value1, out userValue1);
+                if (resultValue1)
+                {
+                    Console.WriteLine("success");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    Console.WriteLine("Enter numbers! press any key to continue");
+                    Console.ReadKey();
+                    continue;
+                }
                 string Value2 = Console.ReadLine();
                 int.TryParse(Value2, out userValue2);
+
+                bool resultValue2 = int.TryParse(Value2, out userValue2);
+                if (resultValue2)
+                {
+                    Console.WriteLine("success");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    Console.WriteLine("Enter numbers! press any key to continue");
+                    Console.ReadKey();
+                    continue;
+                }
                 string Value3 = Console.ReadLine();
                 int.TryParse(Value3, out userValue3);
+                bool resultValue3 = int.TryParse(Value2, out userValue2);
+                if (resultValue3)
+                {
+                    Console.WriteLine("success");
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    Console.WriteLine("Enter numbers! press any key to continue");
+                    Console.ReadKey();
+                    continue;
+                }
+
+
 
 
                 var sumUser = userValue1 + userValue2 + userValue3;
@@ -60,7 +101,7 @@ namespace LearnCsharp
                 else
                 {
                     Console.WriteLine("No luck, try again");
-                    Console.WriteLine("1 number was made up - "+value1); 
+                    Console.WriteLine("1 number was made up - " + value1);
                     Console.WriteLine("2 number was made up - " + value2);
                     Console.WriteLine("3 number was made up - " + value3);
                     Console.WriteLine("Press Enter to try again...");
@@ -72,5 +113,7 @@ namespace LearnCsharp
             }
 
         }
+
+     
     }
 }
